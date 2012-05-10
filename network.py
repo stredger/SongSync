5b6f55c79b4e479c01e0b113be0eef9b7b1cfb34
Stephen Tredger <stredger@gmail.com>
2012-05-10 10:23:30 -0700

    added file to ignore *.pyc

---
+++ network.py
@@ -0,0 +1,164 @@
+# network.py
+
+import socket
+import pickle
+
+PORT = 44444
+MAX_CON = 10
+MAX_READ = 4096
+
+SONG_LIST = "SLst"
+SONG_FILE = "SFle"
+SONG_NUM = "SNum"
+FILE_LEN = "FLen"
+MAX_FILE_SIZE = 999999999 # 1GB - 1B, do it in hex???
+HEADER_LEN = len(SONG_LIST) + len(unicode(MAX_FILE_SIZE)) + 2 # +2 for the ;'s
+HEADER_SEP = ";"
+
+
+# creates a socket for TCP and make reusable
+def create_socket():
+
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	
+	return sock
+
+
+# closes a socket
+def close_socket(sock):
+	sock.close()
+	return
+	
+	
+#
+def make_header_string(num):	
+	
+	if (num > MAX_FILE_SIZE):
+		print "File size to large to send"
+		exit(1)
+	
+	str = unicode(num)
+	while (len(str) < len(unicode(MAX_FILE_SIZE))):
+		str = "0" + str
+	
+	return str
+	
+	
+#
+def create_message(msg_type, data):
+
+	raw_data = pickle.dumps(data)
+	msg_len = make_header_string(len(raw_data))
+	msg = msg_type + HEADER_SEP + msg_len + HEADER_SEP + raw_data
+	#print msg
+	return msg
+
+
+#
+def recv_msg_header(sock):
+
+	head = recv_data_chunk(sock, HEADER_LEN)
+	msg_type = head.split(HEADER_SEP)[0]
+	msg_len = int(head.split(HEADER_SEP)[1])
+	
+	return msg_type, msg_len
+
+
+# binds the socket to internet interface and enables it to listen
+def bind_addr(sock):
+ 
+	#sock.bind( (socket.gethostbyname(socket.gethostname()), PORT) )
+	sock.bind( ('127.0.0.1', PORT) )
+	sock.listen(MAX_CON)
+	return
+
+
+# initiates a connection to the address addr
+def init_connection(sock, addr):
+
+	sock.connect((addr, PORT))
+	return
+
+
+# 
+def wait_for_connection(sock):
+
+	(cli_sock, cli_addr) = sock.accept()	
+
+	return (cli_sock, cli_addr)
+	
+
+# Recieves all data sent to the socket
+# Returns socket contents in a string obj
+def recv_data(sock):
+
+	data = ""
+	try:	
+		chunk = sock.recv(4096)
+		sock.setblocking(0) # there must be a better way of doing this...
+		while (chunk != ""):
+			data += chunk
+			chunk = sock.recv(4096)
+			
+		sock.setblocking(1)	# there must be a better way of doing this...
+	except IOError:
+		None
+	return data
+
+
+#
+def recv_data_size(sock, num_bytes):
+
+	data = ""
+	to_read = num_bytes if num_bytes < MAX_READ else MAX_READ
+	try:	
+		data = sock.recv(to_read)
+		num_bytes -= to_read
+
+		while (num_bytes > 0):
+			to_read = num_bytes if num_bytes < MAX_READ else MAX_READ
+			chunk = sock.recv(to_read)
+			if (chunk == ""):
+				break
+			data += chunk
+			num_bytes -= to_read
+
+	except IOError:
+		None
+		
+	return data
+
+
+#
+def recv_data_chunk(sock, chunk_size):
+
+	chunk = ""	
+	try:
+		chunk = sock.recv(chunk_size)
+	except IOError:
+		None
+		
+	return chunk
+
+
+#select.select(potential_readers, potential_writers, potential_errs, timeout)
+	
+"""
+def main():
+	s = create_socket()
+	
+	
+	#bind_addr(s)
+	#wait_for_connection(s)
+	
+	init_connection(s, socket.gethostbyname(socket.gethostname()) )
+	
+	close_socket(s)
+	
+	return
+
+
+if __name__ == "__main__":
+    main()
+"""
---
+++ readxml.py
@@ -0,0 +1,137 @@
+#readxml.py
+
+import sys
+import os
+
+EXIT_FAIL = 1
+
+TEST_CLI_FILE = "test/cli.xml"
+TEST_SERV_FILE = "test/serv.xml"
+XML_FILE = "/Users/stredger/Music/iTunes/iTunes Music Library.xml"
+
+def open_file(path):
+	
+	#try:
+	f_handle = open(path, 'r')
+		
+	#except IOError as (strerror):
+	#	print "IOError: {1}".format(strerror)
+	#	exit(EXIT_FAIL)
+
+	return f_handle
+
+
+# Pass in a file handle for closing
+def close_file( file_h ):
+	#try:
+	file_h.close()
+	#except AttributeError as (strerror):
+	#	print "AttributeError: {0}".format(strerror)
+	#	exit(EXIT_FAIL)
+	
+	return
+
+
+
+# Traverses an iTunes library XML file getting songs and their
+#	locations in the file system. If we find missing song names
+#	or locations we print them to stdout.
+# Returns a dictionary of (song name, network path) for
+#	all the songs found in the XML file.
+def get_songs(file):
+
+	# holds (song, location) tuples
+	songs = {}
+	
+	# holds songs with no location
+	no_loc = []
+	
+	# holds locaions with no song
+	no_song = []
+			
+	line = file.readline()
+	while (line != ""):
+	
+		# find song names
+		if ('>Name<' in line):
+			song = line.split('<string>')[1].split('</string>')[0]
+			line = file.readline()
+			
+			# we found a name so look for a location
+			while ('</dict>' not in line) and (line != ""):
+			
+				# we found a playlist not a song
+				if ('>Playlist ID<' in line):
+					song = None
+					break
+			
+				if ('>Location<' in line):
+					# we found a location, place the (song, loc) tuple 
+					#	in songs and set song / loc to None so we know
+					loc = line.split('<string>')[1].split('</string>')[0]
+					songs[song] = loc
+					song = loc = None
+					break
+				line = file.readline()
+				
+			# we found no location for the song name!
+			if song is not None:
+				no_loc.append(song)
+			
+		# we found a location before a song name!
+		elif ('>Location<' in line):
+			no_song.append(line.split('<string>')[1].split('</string>')[0])
+			
+		line = file.readline()
+		
+	# print song names with missing locations
+	if len(no_loc):
+		print "We failed to find locations for:"
+		for song in no_loc:
+			print ">> " + song
+	
+	# print locations with missing song names
+	if len(no_song):
+		print "We failed to find song names for:"
+		for loc in no_song:
+			print ">> " + loc.strip()
+	
+	return songs
+
+
+# Computes the songs that appear in serv_song but not cli_song
+# Returns a (name, path) dictionary of the missing songs 
+def compute_missing_songs(serv_songs, cli_songs):
+
+	missing_songs = {}
+	
+	for song in serv_songs:
+		if not (cli_songs.has_key(song)):
+			missing_songs[song] = serv_songs[song]
+	
+	return missing_songs
+
+
+"""
+def main():
+
+	#cli_xml = open_file(XML_FILE)
+
+	cli_xml = open_file(TEST_CLI_FILE)
+	serv_xml = open_file(TEST_SERV_FILE)
+	
+	cli_song_list = get_songs(cli_xml)
+	serv_song_list = get_songs(serv_xml)
+		
+	missing_songs = compute_missing_songs(serv_song_list, cli_song_list)
+	
+		
+	close_file(cli_xml)
+	close_file(serv_xml)
+
+	return
+
+
+if __name__ == "__main__":
+    main()
+"""
